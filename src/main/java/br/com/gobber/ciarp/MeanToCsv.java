0f8693b30b358a849d76b43fa09b32a07a47d3ac
gobber <charles26f@gmail.com>
2017-09-23 01:38:56 -0300

    Projeto com os algoritmos utilizados no CIARP 2017

---
+++ src/main/java/br/com/gobber/ciarp/MeanToCsv.java
@@ -0,0 +1,58 @@
+package br.com.gobber.ciarp;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+
+
+public class MeanToCsv {
+	
+private final String SEPARATOR = ",";
+	
+	private PrintWriter output;
+	
+	private static MeanToCsv instance;
+	
+	public static MeanToCsv createInstance( File file ) {
+		
+		return instance = new MeanToCsv( file );
+		
+	}
+	
+	public static MeanToCsv getInstance() {	
+		
+		return instance;
+		
+	}
+	
+	private MeanToCsv ( File file ) {
+		
+		try {
+			
+			output = new PrintWriter( new FileOutputStream( file ) );
+			
+		} catch ( FileNotFoundException e ) {
+						
+			e.printStackTrace();
+			
+		}	
+		
+	}
+	
+	public void write( String testId, String mean ) {
+			
+		StringBuffer data = new StringBuffer();		
+			
+		data.append( testId + SEPARATOR + mean );
+		
+		output.println( data );
+		
+	}
+	
+	public void destroy() {
+		
+		output.close();
+		
+	}
+
+}
